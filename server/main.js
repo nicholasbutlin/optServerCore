ed65167a3a22aad5b120688b4f575c499f577cfc
Nick Butlin <nicholasbutlin@gmail.com>
2018-01-29 11:28:40 +0000

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 import '/imports/startup/server';
 import { Meteor } from 'meteor/meteor';
 
-// TODO: Settings: Put this in settings file / bash
+// TODO: Settings: Put this in settings file 
 Meteor.startup(() => {
-  process.env.MAIL_URL = '***REMOVED***';
+  process.env.MAIL_URL = '';
 });
